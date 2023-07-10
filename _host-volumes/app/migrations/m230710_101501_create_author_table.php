1b12c91d35f6e098cb861afad0246f84bf2ff7a1
Andrey Ponteleev <ionrussia@gmail.com>
2023-07-10 14:06:43 +0300

    added Book & Author migrations

---
+++ _host-volumes/app/migrations/m230710_101501_create_author_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%author}}`.
+ */
+class m230710_101501_create_author_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%author}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(),
+            'body' => $this->text(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%author}}');
+    }
+}
---
+++ _host-volumes/app/migrations/m230710_105612_create_book_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%book}}`.
+ */
+class m230710_105612_create_book_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%book}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(),
+            'releaseDate' => $this->date(),
+            'description' => $this->text(),
+            'isbn' => $this->integer(10),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%book}}');
+    }
+}
