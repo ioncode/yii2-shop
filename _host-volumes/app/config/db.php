0fd5db45eeac512734723c24002bd0377b91d0b2
Andrey Ponteleev <ionrussia@gmail.com>
2023-07-10 13:05:31 +0300

    added db

--- _host-volumes/app/config/db.php
+++ _host-volumes/app/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=db;dbname=yii2basic_test',
     'username' => 'root',
-    'password' => '',
+    'password' => 'my_root_password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
